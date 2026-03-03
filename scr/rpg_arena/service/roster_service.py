41f35a831a74dff1a1dca0c866c77fd54f21c1d4
merlinkamitz <merlin.kamitz@tu-dortmund.de>
2026-03-03 22:02:04 +0100

    update roster_service to balance between strong and weak units

--- scr/rpg_arena/service/roster_service.py
+++ scr/rpg_arena/service/roster_service.py
@@ -8,62 +8,89 @@ class RosterService:
     def __init__(self, root_service: "RootService"):
         self.root_service = root_service
 
-    def modify_unit_values(self, unit: "Fighter", is_enemy: bool):
+    def modify_unit_values(self, unit: "Fighter", type_: int):
 
-        unit.hp += self.generate_unit_stat_value() + 15
+        unit.hp += self.generate_unit_stat_value(type_) + 15
         unit.max_hp = unit.hp
-        unit.strength += self.generate_unit_stat_value()
-        unit.magic += self.generate_unit_stat_value()
-        unit.skill += self.generate_unit_stat_value()
-        unit.speed += self.generate_unit_stat_value()
-        unit.luck += self.generate_unit_stat_value()
-        unit.defense += self.generate_unit_stat_value()
-        unit.res += self.generate_unit_stat_value()
+        unit.strength += self.generate_unit_stat_value(type_)
+        unit.magic += self.generate_unit_stat_value(type_)
+        unit.skill += self.generate_unit_stat_value(type_)
+        unit.speed += self.generate_unit_stat_value(type_)
+        unit.luck += self.generate_unit_stat_value(type_)
+        unit.defense += self.generate_unit_stat_value(type_)
+        unit.res += self.generate_unit_stat_value(type_)
 
-        unit.hp_growth += self.generate_unit_growth_value()
-        unit.strength_growth += self.generate_unit_growth_value()
-        unit.magic_growth += self.generate_unit_growth_value()
-        unit.skill_growth += self.generate_unit_growth_value()
-        unit.speed_growth += self.generate_unit_growth_value()
-        unit.luck_growth += self.generate_unit_growth_value()
-        unit.defense_growth += self.generate_unit_growth_value()
-        unit.res_growth += self.generate_unit_growth_value()
+        unit.hp_growth += self.generate_unit_growth_value(type_)
+        unit.strength_growth += self.generate_unit_growth_value(type_)
+        unit.magic_growth += self.generate_unit_growth_value(type_)
+        unit.skill_growth += self.generate_unit_growth_value(type_)
+        unit.speed_growth += self.generate_unit_growth_value(type_)
+        unit.luck_growth += self.generate_unit_growth_value(type_)
+        unit.defense_growth += self.generate_unit_growth_value(type_)
+        unit.res_growth += self.generate_unit_growth_value(type_)
 
         return unit
 
-    def generate_unit_growth_value(self):
+    def generate_unit_growth_value(self, type_: int) -> float:
+        """
+        Generate a random growth value based on type:
+        type_ == 1: player units (0.05 - 0.50, tendency to 0.25)
+        type_ == 2: weaker enemies (0.00 - 0.25, tendency to 0.15)
+        type_ == 3: stronger enemies (0.25 - 0.70, tendency to 0.35)
+        """
+        match type_:
+            case 1:
+                range_ = range(1, 11)  # 0.05 .. 0.50
+                center = 0.25
+            case 2:
+                range_ = range(0, 6)  # 0.00 .. 0.25
+                center = 0.15
+            case 3:
+                range_ = range(5, 15)  # 0.25 .. 0.70
+                center = 0.35
+            case _:
+                range_ = range(1, 11)
+                center = 0.25
 
-        # growths should be random between 0.05 and 0.5; but with a tendency to the center of 0.25
-        possible_growths = [i * 0.05 for i in range(1, 11)]
+        possible_growths = [i * 0.05 for i in range_]
 
         weights = []
         for value in possible_growths:
-            distance = abs(value - 0.25)
-            weight = 1 / (1 + distance * 10)
+            distance = abs(value - center)
+            weight = 1 / (1 + distance * len(range_))
             weights.append(weight)
 
         return random.choices(possible_growths, weights=weights, k=1)[0]
 
-    def generate_unit_stat_value(self):
+    def generate_unit_stat_value(self, type_: int) -> int:
+        """
+        Generate a random base stat value for a unit.
+        type_ == 1: player units (0-15, tendency to 5)
+        type_ == 2: weaker enemies (0-10, tendency to 3)
+        type_ == 3: stronger enemies (5-15, tendency to 8)
+        """
+        match type_:
+            case 1:
+                possible_values = list(range(0, 16))
+                center = 5
+            case 2:
+                possible_values = list(range(0, 11))
+                center = 3
+            case 3:
+                possible_values = list(range(5, 16))
+                center = 8
+            case _:
+                possible_values = list(range(0, 16))
+                center = 5
 
-        # stats should be random between 0 and 15; but with a tendency to 5
-        possible_growths = [i * 0.05 for i in range(1, 11)]
-
-        possible_values = list(range(0, 16))  # 0 bis 15
-        weights = []
-        for value in possible_values:
-            # Abstand zur Mitte (5)
-            distance = abs(value - 5)
-
-            weight = 1 / (1 + distance)
-            weights.append(weight)
+        weights = [1 / (1 + abs(value - center)) for value in possible_values]
 
         return random.choices(possible_values, weights=weights, k=1)[0]
 
-    def generate_random_unit(self, is_enemy: bool):
+    def generate_random_unit(self, type_: int):
         random_class = random.choice(list(UnitClass))
         new_fighter = Fighter(random_class)
-        new_fighter = self.modify_unit_values(new_fighter, is_enemy)
+        new_fighter = self.modify_unit_values(new_fighter, type_)
 
         random_weapon = self.random_weapon(random_class)
         new_fighter.items.append(random_weapon)
@@ -81,14 +108,18 @@ class RosterService:
         return random.choice(possible_weapons)
 
     def generate_initial_units(self):
-        units = [self.generate_random_unit(is_enemy= False) for _ in range(5)]
+        units = [self.generate_random_unit(1) for _ in range(5)]
         names = random.sample(fighter_names, 5)
         for i in range(0,5):
             units[i].name = names[i]
         return units
 
     def generate_enemy_units(self):
-        units = [self.generate_random_unit(is_enemy=True) for _ in range(3)]
+        easy_enemy = self.generate_random_unit(2)
+        normal_enemy = self.generate_random_unit(1)
+        hard_enemy =self.generate_random_unit(3)
+
+        units = [easy_enemy, normal_enemy, hard_enemy]
         for i in range(0,3):
             self.level_enemy_unit(units[i], i)
             units[i].name = "Gladiator"
@@ -99,7 +130,7 @@ class RosterService:
 
         match strength:
             case 0:
-                level = round(random.uniform(1, round_))
+                level = round_ - 1 if round_ > 1 else round_
                 unit.level_enemy(level)
                 base_gold = round_ * 100
                 unit.gold = max(50, int(base_gold  + random.normalvariate(0, base_gold * 0.1)))
