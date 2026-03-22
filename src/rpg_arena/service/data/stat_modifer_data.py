d7213f6aab11d8cd35a147e999ef35b5b071e9d7
merlinkamitz <merlin.kamitz@tu-dortmund.de>
2026-03-22 14:07:35 +0100

    update skill prices

--- src/rpg_arena/service/data/stat_modifer_data.py
+++ src/rpg_arena/service/data/stat_modifer_data.py
@@ -25,42 +25,42 @@ crit_plus_15 = StatModifierSkill(
 
 crit_avoid_plus_15 = StatModifierSkill(
     name="Crit Avoid +15",
-    price=1000,
+    price=250,
     target="crt_avoid",
     value=15,
     description = "Increases unit's critical avoidance by 15 points.")
 
 weapon_master = StatModifierSkill(
     name="Weapon Master",
-    price=1000,
+    price=2500,
     target="weight",
     value=0,
     description = "Removes weapon weight penalties for the unit.")
 
 hit_plus_30 = StatModifierSkill(
     name="Hit +30",
-    price=1000,
+    price=1500,
     target="hit",
     value=30,
     description = "Increases unit's hit rate by 30 points.")
 
 avoid_plus_30 = StatModifierSkill(
     name="Avoid +30",
-    price=1000,
+    price=2000,
     target="avoid",
     value=30,
     description = "Increases unit's avoid (evasion) by 30 points.")
 
 crit_plus_30 = StatModifierSkill(
     name="Crit +30",
-    price=1000,
+    price=2000,
     target="crt",
     value=30,
     description = "Increases unit's critical hit rate by 30 points.")
 
 crit_avoid_plus_30 = StatModifierSkill(
     name="Crit Avoid +30",
-    price=1000,
+    price=500,
     target="crt_avoid",
     value=30,
     description = "Increases unit's critical avoidance by 30 points.")
